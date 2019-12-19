e6e30d638714782176f5c0eb6e152377e2880c6f
Anel CC <anel.calvo.cervantes@gmail.com>
2019-12-19 00:38:21 -0500

    update placeholder and error imagen

--- app/src/main/java/com/anelcc/pokedex/ui/PokemonListAdapter.java
+++ app/src/main/java/com/anelcc/pokedex/ui/PokemonListAdapter.java
@@ -33,8 +33,8 @@ public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.
         holder.pokemonName.setText(pokemons.get(position).getName());
 
         Picasso.with(holder.pokemonImage.getContext()).load(imageUrl)
-                .placeholder(R.drawable.ic_error)
-                .error(R.drawable.ic_loading)
+                .placeholder(R.drawable.ic_loading)
+                .error(R.drawable.ic_error)
                 .resize(300, 300)
                 .into(holder.pokemonImage);
     }
