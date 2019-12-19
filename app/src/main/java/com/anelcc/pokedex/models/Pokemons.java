5184162d7645dba87551ec4f46833d289b543b0c
Anel CC <anel.calvo.cervantes@gmail.com>
2019-12-18 21:10:15 -0500

    Create models from response

---
+++ app/src/main/java/com/anelcc/pokedex/models/Pokemons.java
@@ -0,0 +1,16 @@
+package com.anelcc.pokedex.models;
+
+import java.util.ArrayList;
+
+public class Pokemons {
+
+    private ArrayList<Pokemon> pokemon;
+
+    public ArrayList<Pokemon> getPokemon() {
+        return pokemon;
+    }
+
+    public void setPokemon(ArrayList<Pokemon> pokemon) {
+        this.pokemon = pokemon;
+    }
+}
